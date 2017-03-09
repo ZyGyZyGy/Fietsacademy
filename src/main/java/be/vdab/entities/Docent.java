45d6f0ad1e276b568abd9d4c8345b25f9dee44c1
Alex <jegorov.aleksandr@gmail.com>
2017-03-09 09:24:31 +0100

    eerste commit

---
+++ src/main/java/be/vdab/entities/Docent.java
@@ -0,0 +1,51 @@
+package be.vdab.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "docenten")
+public class Docent implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String voornaam;
+    private String familienaam;
+    private BigDecimal wedde;
+    private long rijksRegisterNr;
+
+    public long getId() {
+	return id;
+    }
+
+    public String getVoornaam() {
+	return voornaam;
+    }
+
+    public String getFamilienaam() {
+	return familienaam;
+    }
+
+    public BigDecimal getWedde() {
+	return wedde;
+    }
+
+    public long getRijksRegisterNr() {
+	return rijksRegisterNr;
+    }
+
+    public String getNaam() {
+	return voornaam + " " + familienaam;
+    }
+    
+    
+
+}
