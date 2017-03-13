7c2942f8e048667551ad8fcb68a87cda35c97479
Aleks <Aleks@LOK111-05>
2017-03-13 10:35:58 +0100

    10 ENTITY TOEVOEGEN

--- src/main/java/be/vdab/entities/Docent.java
+++ src/main/java/be/vdab/entities/Docent.java
@@ -2,7 +2,6 @@ package be.vdab.entities;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -27,6 +26,17 @@ public class Docent implements Serializable {
     private long rijksRegisterNr;
     @Enumerated(EnumType.STRING)
     private Geslacht geslacht;
+
+    public Docent(String voornaam, String familienaam, BigDecimal wedde, long rijksRegisterNr, Geslacht geslacht) {
+	this.voornaam = voornaam;
+	this.familienaam = familienaam;
+	this.wedde = wedde;
+	this.rijksRegisterNr = rijksRegisterNr;
+	this.geslacht = geslacht;
+    }
+
+    protected Docent() { // default constructor is verplicht voor JPA
+    }
 
     public long getId() {
 	return id;
@@ -53,7 +63,59 @@ public class Docent implements Serializable {
     }
 
     public Geslacht getGeslacht() {
-        return geslacht;
+	return geslacht;
     }
-    
+
+    public static boolean isVoornaamValid(String voornaam) {
+	return voornaam != null && !voornaam.isEmpty();
+    }
+
+    public static boolean isFamilienaamValid(String familienaam) {
+	return familienaam != null && !familienaam.isEmpty();
+    }
+
+    public static boolean isWeddeValid(BigDecimal wedde) {
+	return wedde != null && wedde.compareTo(BigDecimal.ZERO) >= 0;
+    }
+
+    public static boolean isRijksRegisterNrValid(long rijksRegisterNr) {
+	long getal = rijksRegisterNr / 100;
+	if (rijksRegisterNr / 1_000_000_000 < 50) {
+	    getal += 2_000_000_000;
+	}
+	return rijksRegisterNr % 100 == 97 - getal % 97;
+    }
+
+    public void setVoornaam(String voornaam) {
+	if (!isVoornaamValid(voornaam)) {
+	    throw new IllegalArgumentException();
+	}
+	this.voornaam = voornaam;
+    }
+
+    public void setFamilienaam(String familienaam) {
+	if (!isFamilienaamValid(familienaam)) {
+	    throw new IllegalArgumentException();
+	}
+	this.familienaam = familienaam;
+    }
+
+    public void setWedde(BigDecimal wedde) {
+	if (!isWeddeValid(wedde)) {
+	    throw new IllegalArgumentException();
+	}
+	this.wedde = wedde;
+    }
+
+    public void setGeslacht(Geslacht geslacht) {
+	this.geslacht = geslacht;
+    }
+
+    public void setRijksRegisterNr(long rijksRegisterNr) {
+	if (!isRijksRegisterNrValid(rijksRegisterNr)) {
+	    throw new IllegalArgumentException();
+	}
+	this.rijksRegisterNr = rijksRegisterNr;
+    }
+
 }
