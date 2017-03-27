236f2986ca4afee4c0007eb2e9d97abb2dc336fa
Aleks <Aleks@LOK111-05>
2017-03-27 09:16:53 +0200

    23 DE METHODS EQUALS EN HASHCODE LATEN GENEREREN

--- src/main/java/be/vdab/valueobjects/Adres.java
+++ src/main/java/be/vdab/valueobjects/Adres.java
@@ -39,4 +39,47 @@ public class Adres implements Serializable {
 	return gemeente;
     }
 
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + (gemeente == null ? 0 : gemeente.hashCode());
+	result = prime * result + (huisNr == null ? 0 : huisNr.hashCode());
+	result = prime * result + (postcode == null ? 0 : postcode.hashCode());
+	result = prime * result + (straat == null ? 0 : straat.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (!(obj instanceof Adres))
+	    return false;
+	Adres adres = (Adres) obj;
+	if (gemeente == null) {
+	    if (adres.gemeente != null)
+		return false;
+	} else if (!gemeente.equals(adres.gemeente))
+	    return false;
+	if (huisNr == null) {
+	    if (adres.huisNr != null)
+		return false;
+	} else if (!huisNr.equals(adres.huisNr))
+	    return false;
+	if (postcode == null) {
+	    if (adres.postcode != null)
+		return false;
+	} else if (!postcode.equals(adres.postcode))
+	    return false;
+	if (straat == null) {
+	    if (adres.straat != null)
+		return false;
+	} else if (!straat.equals(adres.straat))
+	    return false;
+	return true;
+    }
+
 }
